14d6ff4129d596c70576e5e875bb687051f0fe8f
zuhaerini <andizuhaerinirr@gmail.com>
2016-12-30 18:08:56 +0800

    Project Hitung Luas Persegi

---
+++ app/src/main/java/com/example/jhpiego/test/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.jhpiego.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText edPanjang, edLebar;
+    private Button btnHitung;
+    private TextView txtLuas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Hitung Luas Persegi Panjang");
+
+        edPanjang = (EditText) findViewById(R.id.ed_panjang);
+        edLebar = (EditText) findViewById(R.id.ed_lebar);
+        btnHitung = (Button) findViewById(R.id.btn_hitung);
+        txtLuas = (TextView) findViewById(R.id.txtluas);
+
+        btnHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Panjang = edPanjang.getText().toString().trim();
+                String Lebar = edLebar.getText().toString().trim();
+
+                double p = Double.parseDouble(Panjang);
+                double l = Double.parseDouble(Lebar);
+
+                double luas = p*l;
+
+                txtLuas.setText("Luas Persegi Panjang adalah "+luas);
+
+            }
+        });
+    }
+}
